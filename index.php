0635ca29b8bc33bba8cdd02e26421392daa8260d
Shahriar-H <67462321+Shahriar-H@users.noreply.github.com>
2021-11-08 09:19:06 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php
+    include './credentials/header.php';
+
+?>
+  
+
+<section class="section_one">
+    <div class="section_row">
+        <div class="two_div dnone_for_mobile">
+            <div class="leftdiv_container">
+                <div class="left_image">
+                    <div class="left_container_title_div">
+                        <h1>Online Loundry Servece in <span class="bangladesh">Bangladesh</span></h1>
+                        <p class="shortdescrip">rem distinctio explicabo,rem distinctio explicabo,rem distinctio explicabo,rem distinctio explicabo, assumenda neque praesentium, blanditiis perspiciatis ab iste sint excepturi eos labore aut tenetur!</p>
+                        <div class="doanload_app_div">
+                            <a href="">
+                                <img  src="./images/google_play.png" alt="google_play">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- right side -->
+        <div class="two_div login_div">
+            <div class="leftdiv_container">
+                <div class="right_div_container">
+                    <div class="right_content_div form_content">
+                        <div id="dynamic_div">
+                            <form class="text-right otpForm" action="">
+                                <div class="input_div">
+                                    <input type="number" name="phone_number" placeholder="Phone Number"><br>
+                                </div>
+                                <input class="btn btn-danger btn-sm send_otp" type="submit" name="submit_number" value="SEND OTP">
+                            </form>
+                            <div class="continue_btn">
+                                <button>CONTINUE</button>
+                            </div>
+                        </div>
+
+                        <div class="nav_bar">
+                            <ul>
+                                <li id="home"><a href="" class="active_menu"><i class="fas fa-home"></i> </a></li>
+                                <li id="list"><a href="#"><i class="fas fa-clipboard-list"></i> </a></li>
+                                <li id="add"><a href="#"><i class="fas fa-plus-square"></i> </a></li>
+                                <li id="user"><a href="#"><i class="fas fa-user"></i> </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- display for mobile -->
+
+        <div class="two_div dnone_for_pc">
+            <div class="leftdiv_container">
+                <div class="left_image">
+                    <div class="left_container_title_div">
+                        <h1>Online Loundry Servece in <span class="bangladesh">Bangladesh</span></h1>
+                        <p class="shortdescrip">rem distinctio explicabo,rem distinctio explicabo,rem distinctio explicabo,rem distinctio explicabo, assumenda neque praesentium, blanditiis perspiciatis ab iste sint excepturi eos labore aut tenetur!</p>
+                        <div class="doanload_app_div">
+                            <a href="">
+                                <img  src="./images/google_play.png" alt="google_play">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+    include './credentials/footer.php';
+
+?>
